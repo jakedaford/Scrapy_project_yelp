cd88b465759558e89bdc38e5f74f512b50c2870a
Jake Ralston <jakedaford@gmail.com>
2018-05-10 17:34:24 -0400

    can find number of reviews for each restaurant in debug mode settings

--- yelp/yelp/spiders/yelp_spider.py
+++ yelp/yelp/spiders/yelp_spider.py
@@ -13,7 +13,7 @@ class YelpSpider(Spider):
 		_, _, total = map(lambda x: int(x), re.findall('\d+', text))
 		
 		# list comprehension to construct all the urls
-		result_urls = ['https://www.yelp.com/search?find_desc=indian&find_loc=10001&start=' + str(x) for x in range(0, total, 10)]
+		result_urls = ['https://www.yelp.com/search?find_desc=indian&find_loc=10001&start=' + str(x) for x in range(0, 2, 10)]
 
 		# Yield the requests to different search result urls, 
 		# using parse_result_page function to parse the response.
@@ -24,5 +24,36 @@ class YelpSpider(Spider):
 		# This function parses the search result page
 
 		# We are looking for the url of the detail page
-		''' NOT DONE WITH THIS YET NEED TO USE GET_ATTRIBUTE TO GRAB THE HREF'''
-		restaurant_urls = response.xpath('//a[@class="biz-name js-analytics-click"]')
+		restaurant_urls_ending = response.xpath('//a[@class="biz-name js-analytics-click"]/@href').extract()[1:]
+		
+		# Manually concatenate all the urls
+		restaurant_urls = ['https://www.yelp.com' + url for url in restaurant_urls_ending]
+
+		# Yield the requests to the restaurant pages,
+		# using parse_restaurant_page function to parse the response
+		for url in restaurant_urls:
+			yield Request(url=url, callback=self.parse_restaurant_page)
+
+	def parse_restaurant_page(self, response):
+		# Find the total number of reviews so that we can decide how many urls to scrape next
+		text = response.xpath('//span[@class = "review-count rating-qualifier"]/text()').extract_first()
+		total = map(lambda x: int(x), re.findall('\d+', text))
+		print(list(total))
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
